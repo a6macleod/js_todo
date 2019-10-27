eb7b58f090da107bfbfd029431d7f077569bdb36
Andrew MacLeod <A6macleod@gmail.com>
2019-10-26 22:12:20 -0400

    initial commit file set up

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* 
+To do should have
+  start with a default project
+  title
+  description
+  dueDate
+  priority
+  notes
+  a subchecklist
+
+Functionality
+  create a new project
+  Mark complete project compelete
+  Unmark project complete back to to do
+  delete the project
+  Local Storage 
+*/
